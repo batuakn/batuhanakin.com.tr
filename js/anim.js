980bf5182b8e38f776b4e7a56ed1d2817b1da892
Batuhan Akın <batuakn@hotmail.com>
2018-09-01 18:11:14 +0300

    Update anim.js

--- js/anim.js
+++ js/anim.js
@@ -1,105 +1,39 @@
-// Cache gallery container
-const galleryContainer = document.querySelector('.react-gallery');
-
-// Create new array with URLs for images
-let imgUrls = [
-  'https://source.unsplash.com/3Z70SDuYs5g/800x600',
-  'https://source.unsplash.com/01vFmYAOqQ0/800x600',
-  'https://source.unsplash.com/2Bjq3A7rGn4/800x600',
-  'https://source.unsplash.com/t20pc32VbrU/800x600',
-  'https://source.unsplash.com/pHANr-CpbYM/800x600',
-  'https://source.unsplash.com/3PmwYw2uErY/800x600',
-  'https://source.unsplash.com/uOi3lg8fGl4/800x600',
-  'https://source.unsplash.com/CwkiN6_qpDI/800x600',
-  'https://source.unsplash.com/9O1oQ9SzQZQ/800x600',
-  'https://source.unsplash.com/E4944K_4SvI/800x600',
-  'https://source.unsplash.com/-hI5dX2ObAs/800x600',
-  'https://source.unsplash.com/vZlTg_McCDo/800x600'
-];
-
-// Component for gallery image
-class GalleryImage extends React.Component {
-  render() {
-    return(
-      <img className={this.props.className} src={this.props.src} alt={this.props.alt} />
-    )
-  }
-}
-
-// Component for gallery modal
-class GalleryModal extends React.Component {
-  render() {
-    if (this.props.isOpen === false) {
-      return null;
-    }
-    
-    return(
-      <div isOpen={this.props.isOpen} className='modal-overlay' onClick={this.props.onClick} name={this.props.name}>
-        <div className='modal-body'>
-          <a className='modal-close' href='#' onClick={this.props.onClick}><span className='fa fa-times'></span></a>
-          
-          <img src={this.props.src} />
-        </div>
-      </div>
-    )
-  }
-}
-
-// Component for gallery
-class Gallery extends React.Component{
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      showModal: false,
-      url: ''
-    }
-    
-    this.openModal = this.openModal.bind(this);
-    
-    this.closeModal = this.closeModal.bind(this);
-  }
+$(function() {
+  $(".img-w").each(function() {
+    $(this).wrap("<div class='img-c'></div>")
+    let imgSrc = $(this).find("img").attr("src");
+     $(this).css('background-image', 'url(' + imgSrc + ')');
+  })
+            
   
-  render() {
-    return(
-      <div refs='gallery-container' className='container-fluid gallery-container'>
-        <div className='row'>
-          {
-            imgUrls.map((url, index) => {
-               return <div className='col-sm-6 col-md-3 col-xl-2'>
-                  <div className='gallery-card'>
-                    <GalleryImage className='gallery-thumbnail' src={url} alt={'Image number ' + (index + 1)} />
-                    
-                    <span className='card-icon-open fa fa-expand' value={url} onClick={(e) => this.openModal(url, e)}></span>
-                  </div>
-                </div>
-             })
-           }
-        </div>
-        
-        <GalleryModal isOpen={this.state.showModal} onClick={this.closeModal} src={this.state.url} /> 
-      </div>
-    )
-  }
+  $(".img-c").click(function() {
+    let w = $(this).outerWidth()
+    let h = $(this).outerHeight()
+    let x = $(this).offset().left
+    let y = $(this).offset().top
+    
+    
+    $(".active").not($(this)).remove()
+    let copy = $(this).clone();
+    copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
+    $(".active").css('top', y - 8);
+    $(".active").css('left', x - 8);
+    
+      setTimeout(function() {
+    copy.addClass("positioned")
+  }, 0)
+    
+  }) 
   
-  // Function for opening modal dialog
-  openModal(url, e) {
-     this.setState({
-       showModal: true,
-       url: url
-     })
-   };
+  
 
-  // Function for closing modal dialog
-  closeModal() {
-    this.setState({
-      showModal: false,
-      url: ''
-    })
-  }
-}
+  
+})
 
-// Let's render the whole thing
-ReactDOM.render(
-  <Gallery imgUrls={imgUrls} />
-, galleryContainer);
+$(document).on("click", ".img-c.active", function() {
+  let copy = $(this)
+  copy.removeClass("positioned active").addClass("postactive")
+  setTimeout(function() {
+    copy.remove();
+  }, 500)
+})
